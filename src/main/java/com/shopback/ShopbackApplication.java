0d53185c60257284d20dd831625f3258885b97c1
gyeonghui lee <gmana802@gmail.com>
2025-07-25 16:49:29 +0900

    test aws

---
+++ src/main/java/com/shopback/ShopbackApplication.java
@@ -0,0 +1,13 @@
+package com.shopback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopback/ShopbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
